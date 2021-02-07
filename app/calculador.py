d9899b0698acbf495c1c7630a65f8c945952d5c0
felipeucelli <78266261+felipeucelli@users.noreply.github.com>
2021-02-06 20:54:51 -0400

    Update calculador.py

--- app/calculador.py
+++ app/calculador.py
@@ -3,86 +3,32 @@ from math import sqrt
 
 def calcular(screen):
     screen_tot = screen.get()
-    if len(screen_tot) > 1 and screen_tot[0] != '+' and screen_tot[0] != '-' and screen_tot[0] != 'x'\
-            and screen_tot[0] != '/' and screen_tot[0] != '^' and screen_tot[0] != '.':
-        if '+' in screen_tot and '√' not in screen_tot:
-            if screen_tot.count('+') == 1:
-                result = screen_tot.split('+')
-                if result[1] != '':
-                    result_screen = str(float(result[0]) + float(result[1]))
-                    for c in result_screen.split('.')[1]:
-                        if c != '0':
-                            screen.set(str(result_screen))
-                            break
-                        else:
-                            screen.set(str(result_screen.split('.')[0]))
+    valid = False
+    if '^' in screen_tot:
+        screen_tot = screen_tot.replace('^', '**')
+    if 'x' in screen_tot:
+        screen_tot = screen_tot.replace('x', '*')
+    if screen_tot[0] != '+' and screen_tot[0] != '*' and screen_tot[0] != '/' \
+            and screen_tot[0] != '^' and screen_tot[0] != '.':
+        for c in screen_tot:
+            if not c.isalpha():
+                valid = True
             else:
-                screen.set('Syntax error')
-        if '-' in screen_tot and '√' not in screen_tot:
-            if screen_tot.count('-') == 1:
-                result = screen_tot.split('-')
-                if result[1] != '':
-                    result_screen = str(float(result[0]) - float(result[1]))
-                    for c in result_screen.split('.')[1]:
-                        if c != '0':
-                            screen.set(str(result_screen))
-                            break
-                    else:
-                        screen.set(str(result_screen.split('.')[0]))
+                valid = False
+                break
+        if valid and '++' not in screen_tot and '--' not in screen_tot and 'xx' not in screen_tot \
+                and '//' not in screen_tot and '√√' not in screen_tot and '..' not in screen_tot:
+            if screen_tot[0] == '√':
+                if '+' in screen_tot or '-' in screen_tot or '*' in screen_tot or '/' in screen_tot \
+                        or '**' in screen_tot:
+                    square_root = eval(screen_tot.split('√')[1])
+                    screen.set(sqrt(int(square_root)))
+                else:
+                    square_root = screen_tot.split('√')[1]
+                    screen_tot.set(sqrt(square_root))
             else:
-                screen.set('Syntax error')
-        if 'x' in screen_tot and '√' not in screen_tot:
-            if screen_tot.count('x') == 1:
-                result = screen_tot.split('x')
-                if result[1] != '':
-                    result_screen = str(float(result[0]) * float(result[1]))
-                    for c in result_screen.split('.')[1]:
-                        if c != '0':
-                            screen.set(str(result_screen))
-                            break
-                        else:
-                            screen.set(str(result_screen.split('.')[0]))
-            else:
-                screen.set('Syntax error')
-        if '/' in screen_tot and '√' not in screen_tot:
-            if screen_tot.count('/') == 1:
-                result = screen_tot.split('/')
-                if result[1] != '':
-                    result_screen = str(float(result[0]) / float(result[1]))
-                    for c in result_screen.split('.')[1]:
-                        if c != '0':
-                            screen.set(str(result_screen))
-                            break
-                        else:
-                            screen.set(str(result_screen.split('.')[0]))
-            else:
-                screen.set('Syntax error')
-        if screen_tot[0] == '√' and '+' not in screen_tot and '-' not in screen_tot and 'x' not in screen_tot \
-                and '/' not in screen_tot and '^' not in screen_tot and '.' not in screen_tot:
-            if screen_tot.count('√') == 1:
-                result = screen_tot.split('√')
-                if result[1] != '':
-                    result_screen = str(sqrt(float(result[1])))
-                    for c in result_screen.split('.')[1]:
-                        if c != '0':
-                            screen.set(str(result_screen))
-                            break
-                        else:
-                            screen.set(str(result_screen.split('.')[0]))
-            else:
-                screen.set('Syntax error')
-        if '^' in screen_tot and '√' not in screen_tot:
-            if screen_tot.count('^') == 1:
-                result = screen_tot.split('^')
-                if result[1] != '':
-                    result_screen = str(float(result[0]) ** float(result[1]))
-                    for c in result_screen.split('.')[1]:
-                        if c != '0':
-                            screen.set(str(result_screen))
-                            break
-                        else:
-                            screen.set(str(result_screen.split('.')[0]))
-            else:
-                screen.set('Syntax error')
+                screen.set(eval(screen_tot))
+        else:
+            screen.set('Error')
     else:
-        screen.set('Syntax error')
+        screen.set('Error')
--- app/calculadora.py
+++ app/calculadora.py
@@ -4,14 +4,14 @@ from .calculador import calcular
 
 def add_screen(bnt_press, screen):
     value = screen.get()
-    if value != 'Syntax error':
+    if value != 'Error':
         valor = screen.get()
         valor += bnt_press
         screen.set(valor)
 
 
 def remove(screen):
-    if screen.get() != 'Syntax error':
+    if screen.get() != 'Error':
         valor = screen.get()[0:-1]
         screen.set(valor)
 
@@ -29,60 +29,60 @@ def interface(root):
 
     screen = tkinter.StringVar()
 
-    label_screen = tkinter.Entry(root, font='Arial 20', bd=2, relief='solid', justify='right',
+    input_screen = tkinter.Entry(root, font='Arial 20', bd=2, relief='solid', justify='right',
                                  width=18, textvariable=screen, exportselection=0)
 
-    square = tkinter.Button(root, font='Arial 20', width=4, text='√', command=lambda: add_screen('√', screen))
-    expo = tkinter.Button(root, font='Arial 20', width=4, text='x²', command=lambda: add_screen('^', screen))
-    clear = tkinter.Button(root, font='Arial 20', width=4, text='C', command=lambda: screen.set(''))
-    delete = tkinter.Button(root, font='Arial 20', width=4, text='<-', command=lambda: remove(screen))
+    bnt_square_root = tkinter.Button(root, font='Arial 20', width=4, text='√', command=lambda: add_screen('√', screen))
+    bnt_exponent = tkinter.Button(root, font='Arial 20', width=4, text='x²', command=lambda: add_screen('^', screen))
+    bnt_clear = tkinter.Button(root, font='Arial 20', width=4, text='C', command=lambda: screen.set(''))
+    bnt_delete = tkinter.Button(root, font='Arial 20', width=4, text='<-', command=lambda: remove(screen))
 
-    bnt9 = tkinter.Button(root, font='Arial 20', width=4, text=9, command=lambda: add_screen('9', screen))
-    bnt8 = tkinter.Button(root, font='Arial 20', width=4, text=8, command=lambda: add_screen('8', screen))
-    bnt7 = tkinter.Button(root, font='Arial 20', width=4, text=7, command=lambda: add_screen('7', screen))
-    div = tkinter.Button(root, font='Arial 20', width=4, text='/', command=lambda: add_screen('/', screen))
+    bnt_9 = tkinter.Button(root, font='Arial 20', width=4, text=9, command=lambda: add_screen('9', screen))
+    bnt_8 = tkinter.Button(root, font='Arial 20', width=4, text=8, command=lambda: add_screen('8', screen))
+    bnt_7 = tkinter.Button(root, font='Arial 20', width=4, text=7, command=lambda: add_screen('7', screen))
+    bnt_division = tkinter.Button(root, font='Arial 20', width=4, text='/', command=lambda: add_screen('/', screen))
 
-    bnt6 = tkinter.Button(root, font='Arial 20', width=4, text=6, command=lambda: add_screen('6', screen))
-    bnt5 = tkinter.Button(root, font='Arial 20', width=4, text=5, command=lambda: add_screen('5', screen))
-    bnt4 = tkinter.Button(root, font='Arial 20', width=4, text=4, command=lambda: add_screen('4', screen))
-    times = tkinter.Button(root, font='Arial 20', width=4, text='X', command=lambda: add_screen('x', screen))
+    bnt_6 = tkinter.Button(root, font='Arial 20', width=4, text=6, command=lambda: add_screen('6', screen))
+    bnt_5 = tkinter.Button(root, font='Arial 20', width=4, text=5, command=lambda: add_screen('5', screen))
+    bnt_4 = tkinter.Button(root, font='Arial 20', width=4, text=4, command=lambda: add_screen('4', screen))
+    bnt_times = tkinter.Button(root, font='Arial 20', width=4, text='X', command=lambda: add_screen('x', screen))
 
-    bnt3 = tkinter.Button(root, font='Arial 20', width=4, text=3, command=lambda: add_screen('3', screen))
-    bnt2 = tkinter.Button(root, font='Arial 20', width=4, text=2, command=lambda: add_screen('2', screen))
-    bnt1 = tkinter.Button(root, font='Arial 20', width=4, text=1, command=lambda: add_screen('1', screen))
-    minus = tkinter.Button(root, font='Arial 20', width=4, text='-', command=lambda: add_screen('-', screen))
+    bnt_3 = tkinter.Button(root, font='Arial 20', width=4, text=3, command=lambda: add_screen('3', screen))
+    bnt_2 = tkinter.Button(root, font='Arial 20', width=4, text=2, command=lambda: add_screen('2', screen))
+    bnt_1 = tkinter.Button(root, font='Arial 20', width=4, text=1, command=lambda: add_screen('1', screen))
+    bnt_minus = tkinter.Button(root, font='Arial 20', width=4, text='-', command=lambda: add_screen('-', screen))
 
-    dot = tkinter.Button(root, font='Arial 20', width=4, text='.', command=lambda: add_screen('.', screen))
-    bnt0 = tkinter.Button(root, font='Arial 20', width=4, text=0, command=lambda: add_screen('0', screen))
-    equal = tkinter.Button(root, font='Arial 20', width=4, text='=', command=lambda: calcular(screen))
-    plus = tkinter.Button(root, font='Arial 20', width=4, text='+', command=lambda: add_screen('+', screen))
+    bnt_dot = tkinter.Button(root, font='Arial 20', width=4, text='.', command=lambda: add_screen('.', screen))
+    bnt_0 = tkinter.Button(root, font='Arial 20', width=4, text=0, command=lambda: add_screen('0', screen))
+    bnt_equal = tkinter.Button(root, font='Arial 20', width=4, text='=', command=lambda: calcular(screen))
+    bnt_plus = tkinter.Button(root, font='Arial 20', width=4, text='+', command=lambda: add_screen('+', screen))
 
-    label_screen.grid(row=0, columnspan=4, sticky='we')
+    input_screen.grid(row=0, columnspan=4, sticky='we')
 
-    square.grid(row=1, column=0)
-    expo.grid(row=1, column=1)
-    clear.grid(row=1, column=2)
-    delete.grid(row=1, column=3)
+    bnt_square_root.grid(row=1, column=0)
+    bnt_exponent.grid(row=1, column=1)
+    bnt_clear.grid(row=1, column=2)
+    bnt_delete.grid(row=1, column=3)
 
-    bnt9.grid(row=2, column=2)
-    bnt8.grid(row=2, column=1)
-    bnt7.grid(row=2, column=0)
-    div.grid(row=2, column=3)
+    bnt_9.grid(row=2, column=2)
+    bnt_8.grid(row=2, column=1)
+    bnt_7.grid(row=2, column=0)
+    bnt_division.grid(row=2, column=3)
 
-    bnt6.grid(row=3, column=2)
-    bnt5.grid(row=3, column=1)
-    bnt4.grid(row=3, column=0)
-    times.grid(row=3, column=3)
+    bnt_6.grid(row=3, column=2)
+    bnt_5.grid(row=3, column=1)
+    bnt_4.grid(row=3, column=0)
+    bnt_times.grid(row=3, column=3)
 
-    bnt3.grid(row=4, column=2)
-    bnt2.grid(row=4, column=1)
-    bnt1.grid(row=4, column=0)
-    minus.grid(row=4, column=3)
+    bnt_3.grid(row=4, column=2)
+    bnt_2.grid(row=4, column=1)
+    bnt_1.grid(row=4, column=0)
+    bnt_minus.grid(row=4, column=3)
 
-    dot.grid(row=5, column=0)
-    bnt0.grid(row=5, column=1)
-    equal.grid(row=5, column=2)
-    plus.grid(row=5, column=3)
+    bnt_dot.grid(row=5, column=0)
+    bnt_0.grid(row=5, column=1)
+    bnt_equal.grid(row=5, column=2)
+    bnt_plus.grid(row=5, column=3)
 
 
 def sair(root):
