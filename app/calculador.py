77d482686b65017a97853e0a378cc7ddabeb23bc
felipeucelli <78266261+felipeucelli@users.noreply.github.com>
2021-02-08 20:12:42 -0400

    Update calculador.py

--- app/calculador.py
+++ app/calculador.py
@@ -39,33 +39,34 @@ def valid_operation(screen_tot, screen, historic):
 def math_operation(screen):
     historic = screen.get()
     screen_tot = screen.get()
-    if ' ' in historic:
-        historic = historic.replace(' ', '')
-    if ' ' in screen_tot:
-        screen_tot = screen_tot.replace(' ', '')
-
-    if valid_operation(screen_tot, screen, historic):
-        if '^' in screen_tot:
-            screen_tot = screen_tot.replace('^', '**')
-        if 'x' in screen_tot:
-            screen_tot = screen_tot.replace('x', '*')
-        if screen_tot[0] == '√' and screen_tot[1].isnumeric():
-            if '+' in screen_tot or '-' in screen_tot or '*' in screen_tot or '/' in screen_tot \
-                    or '**' in screen_tot:
-                square_root = eval(screen_tot.split('√')[1])
-                screen.set(sqrt(int(square_root)))
-                add_historic(historic + ' = ' + str(sqrt(int(square_root))))
+    if screen_tot != '':
+        if ' ' in historic:
+            historic = historic.replace(' ', '')
+        if ' ' in screen_tot:
+            screen_tot = screen_tot.replace(' ', '')
+        if valid_operation(screen_tot, screen, historic):
+            if '^' in screen_tot:
+                screen_tot = screen_tot.replace('^', '**')
+            if 'x' in screen_tot:
+                screen_tot = screen_tot.replace('x', '*')
+            if screen_tot[0] == '√' and screen_tot[1].isnumeric():
+                if '+' in screen_tot or '-' in screen_tot or '*' in screen_tot or '/' in screen_tot \
+                        or '**' in screen_tot:
+                    square_root = eval(screen_tot.split('√')[1])
+                    screen.set(sqrt(int(square_root)))
+                    add_historic(historic + ' = ' + str(sqrt(int(square_root))))
+                else:
+                    square_root = screen_tot.split('√')[1]
+                    screen.set(sqrt(int(square_root)))
+                    add_historic(historic + ' = ' + str(sqrt(int(square_root))))
+            elif screen_tot[len(screen_tot) - 1].isnumeric() and '√' not in screen_tot:
+                if '+' in screen_tot or '-' in screen_tot or '*' in screen_tot or '/' in screen_tot \
+                        or '**' in screen_tot:
+                    screen.set(eval(screen_tot))
+                    add_historic(historic + ' = ' + str(eval(screen_tot)))
             else:
-                square_root = screen_tot.split('√')[1]
-                screen.set(sqrt(int(square_root)))
-                add_historic(historic + ' = ' + str(sqrt(int(square_root))))
-        elif screen_tot[len(screen_tot) - 1].isnumeric() and '√' not in screen_tot:
-            screen.set(eval(screen_tot))
-            add_historic(historic + ' = ' + str(eval(screen_tot)))
+                screen.set('Error')
+                add_historic(historic + ' = Error')
         else:
             screen.set('Error')
             add_historic(historic + ' = Error')
-    else:
-        screen.set('Error')
-        add_historic(historic + ' = Error')
-
