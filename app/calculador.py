a03a1ad38ae287c5af3483d5b470149d169c2b52
felipeucelli <78266261+felipeucelli@users.noreply.github.com>
2021-02-09 21:38:54 -0400

    Update calculador.py

--- app/calculador.py
+++ app/calculador.py
@@ -18,7 +18,7 @@ def valid_operation(screen_tot, screen, historic):
             for k, v in enumerate(screen_tot):
                 if not v.isnumeric() and screen_tot[0] != '√':
                     if screen_tot[len(screen_tot) - 1].isnumeric():
-                        if screen_tot[k + 1].isnumeric():
+                        if screen_tot[k + 1].isnumeric() or screen_tot[k + 1] == '-':
                             validate_expression = True
                         else:
                             validate_expression = False
@@ -53,12 +53,16 @@ def math_operation(screen):
                 if '+' in screen_tot or '-' in screen_tot or '*' in screen_tot or '/' in screen_tot \
                         or '**' in screen_tot:
                     square_root = eval(screen_tot.split('√')[1])
-                    screen.set(sqrt(int(square_root)))
-                    add_historic(historic + ' = ' + str(sqrt(int(square_root))))
+                    if str(square_root)[0] != '-':
+                        screen.set(sqrt(float(square_root)))
+                        add_historic(historic + ' = ' + str(sqrt(float(square_root))))
+                    else:
+                        screen.set('Error')
+                        add_historic(historic + ' = Error')
                 else:
                     square_root = screen_tot.split('√')[1]
-                    screen.set(sqrt(int(square_root)))
-                    add_historic(historic + ' = ' + str(sqrt(int(square_root))))
+                    screen.set(sqrt(float(square_root)))
+                    add_historic(historic + ' = ' + str(sqrt(float(square_root))))
             elif screen_tot[len(screen_tot) - 1].isnumeric() and '√' not in screen_tot:
                 if '+' in screen_tot or '-' in screen_tot or '*' in screen_tot or '/' in screen_tot \
                         or '**' in screen_tot:
