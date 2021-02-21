9b3a8fad5ed7ce441f8e3dbf03c07242674c2dda
felipeucelli <78266261+felipeucelli@users.noreply.github.com>
2021-02-20 21:11:57 -0400

    Update calculador.py

--- app/calculador.py
+++ app/calculador.py
@@ -6,12 +6,18 @@ class Calculador:
     def __init__(self, screen, control):
         self.screen = screen
         self.control = control
-        self.historic = self.screen.get()
+
+    def duplicate_analysis(self):
+        analysis = True
+        for k, v in enumerate(self.screen.get()):
+            if not v.isnumeric():
+                if len(self.screen.get()) > k + 1:
+                    if self.screen.get()[k + 1] == v:
+                        analysis = False
+        return analysis
 
     def validate_characters(self):
-        if '++' not in self.screen.get() and '--' not in self.screen.get() and 'xx' not in self.screen.get() \
-                and '//' not in self.screen.get() and '√√' not in self.screen.get() and '..' not in self.screen.get() \
-                and '^^' not in self.screen.get() and '!!' not in self.screen.get() and '%%' not in self.screen.get():
+        if self.duplicate_analysis():
             validate_expression = False
             for k, v in enumerate(self.screen.get()):
                 if not v.isnumeric() and self.screen.get()[0] != '√' and \
@@ -71,6 +77,37 @@ class Calculador:
             else:
                 return False
 
+    def calculate_square_root(self):
+        if len(self.screen.get()) > 1:
+            if self.screen.get().count('√') == 1:
+                if not self.screen.get().split('√')[1].isnumeric() and self.screen.get()[1].isnumeric():
+                    if '!' in self.screen.get():
+                        if self.screen.get()[len(self.screen.get()) - 1] == '!':
+                            screen_get_tot = sqrt(float(self.calculate_factorial(
+                                self.screen.get().split('√')[1])))
+                        else:
+                            screen_get_tot = 'Error'
+                    elif '%' in self.screen.get():
+                        if self.screen.get()[len(self.screen.get()) - 1] == '%':
+                            screen_get_tot = sqrt(float(self.calculate_percentage(
+                                self.screen.get().split('√')[1])))
+                        else:
+                            screen_get_tot = 'Error'
+                    else:
+                        square_root = eval(self.screen.get().split('√')[1])
+                        if str(square_root)[0] != '-':
+                            screen_get_tot = sqrt(float(square_root))
+                        else:
+                            screen_get_tot = 'Error'
+                else:
+                    square_root = self.screen.get().split('√')[1]
+                    screen_get_tot = sqrt(float(square_root))
+            else:
+                screen_get_tot = 'Error'
+        else:
+            screen_get_tot = 'Error'
+        return screen_get_tot
+
     @staticmethod
     def calculate_factorial(screen):
         factorial_ok = True
@@ -101,69 +138,46 @@ class Calculador:
         first_value = screen.split(percentage_include + '%')[0]
         percentage_conversion = float(percentage_include) / 100
         convert_first_value = first_value.replace(first_value[len(first_value) - 1], '')
-        percentage_tot = float(convert_first_value) * float(percentage_conversion)
+        if not convert_first_value.isnumeric():
+            operation_first_value = eval(first_value.replace(first_value[len(first_value) - 1], ''))
+            percentage_tot = float(operation_first_value) * float(percentage_conversion)
+        else:
+            percentage_tot = float(convert_first_value) * float(percentage_conversion)
         return eval(first_value + str(percentage_tot))
 
+    def convert_symbols(self):
+        if '^' in self.screen.get():
+            self.screen.set(self.screen.get().replace('^', '**'))
+        if 'x' in self.screen.get():
+            self.screen.set(self.screen.get().replace('x', '*'))
+        if '(' in self.screen.get():
+            for k, _ in enumerate(self.screen.get()):
+                if len(self.screen.get()) > k + 1:
+                    if self.screen.get()[k].isnumeric() and self.screen.get()[k + 1] == '(':
+                        self.screen.set(self.screen.get().replace('(', '*('))
+        if ')' in self.screen.get():
+            for k, _ in enumerate(self.screen.get()):
+                if len(self.screen.get()) > k + 1:
+                    if self.screen.get()[k] == ')' and self.screen.get()[k + 1].isnumeric():
+                        self.screen.set(self.screen.get().replace(')', ')*'))
+
     def math_operation(self):
+        historic = self.screen.get()
         if self.screen.get() != '' and 'e' not in self.screen.get() and not self.screen.get().isnumeric():
-            if ' ' in self.historic:
-                self.historic = self.historic.replace(' ', '')
+            if ' ' in historic:
+                historic = historic.replace(' ', '')
             if ' ' in self.screen.get():
                 self.screen.set(self.screen.get().replace(' ', ''))
             if self.valid_operation():
                 screen_get_tot = ''
-                if '^' in self.screen.get():
-                    self.screen.set(self.screen.get().replace('^', '**'))
-                if 'x' in self.screen.get():
-                    self.screen.set(self.screen.get().replace('x', '*'))
-                if '(' in self.screen.get():
-                    for k, _ in enumerate(self.screen.get()):
-                        if len(self.screen.get()) > k + 1:
-                            if self.screen.get()[k].isnumeric() and self.screen.get()[k + 1] == '(':
-                                self.screen.set(self.screen.get().replace('(', '*('))
-                if ')' in self.screen.get():
-                    for k, _ in enumerate(self.screen.get()):
-                        if len(self.screen.get()) > k + 1:
-                            if self.screen.get()[k] == ')' and self.screen.get()[k + 1].isnumeric() and \
-                                    self.screen.get()[len(self.screen.get()) - 1] != ')':
-                                self.screen.set(self.screen.get().replace(')', ')*'))
+                self.convert_symbols()
                 if self.screen.get()[0] == '√':
-                    if len(self.screen.get()) > 1:
-                        if self.screen.get().count('√') == 1:
-                            if '+' in self.screen.get() or '-' in self.screen.get() or '*' in self.screen.get() or \
-                                    '/' in self.screen.get() or '**' in self.screen.get() or \
-                                    '!' in self.screen.get() and self.screen.get()[1].isnumeric():
-                                if '!' in self.screen.get():
-                                    if self.screen.get()[len(self.screen.get()) - 1] == '!':
-                                        screen_get_tot = sqrt(float(self.calculate_factorial(
-                                            self.screen.get().split('√')[1])))
-                                    else:
-                                        screen_get_tot = 'Error'
-                                elif '%' in self.screen.get():
-                                    if self.screen.get()[len(self.screen.get()) - 1] == '%':
-                                        screen_get_tot = sqrt(float(self.calculate_percentage(
-                                            self.screen.get().split('√')[1])))
-                                    else:
-                                        screen_get_tot = 'Error'
-                                else:
-                                    square_root = eval(self.screen.get().split('√')[1])
-                                    if str(square_root)[0] != '-':
-                                        screen_get_tot = sqrt(float(square_root))
-                                    else:
-                                        screen_get_tot = 'Error'
-                            else:
-                                square_root = self.screen.get().split('√')[1]
-                                screen_get_tot = sqrt(float(square_root))
-                        else:
-                            screen_get_tot = 'Error'
-                    else:
-                        screen_get_tot = 'Error'
+                    screen_get_tot = self.calculate_square_root()
                 elif self.screen.get()[len(self.screen.get()) - 1] == '!':
-                    screen_get_tot = self.calculate_factorial()
-                elif self.screen.get()[len(self.screen.get()) - 1].isnumeric() and '√' not in self.screen.get() or \
-                        self.screen.get()[len(self.screen.get()) - 1] == ')':
-                    if '+' in self.screen.get() or '-' in self.screen.get() or '*' in self.screen.get() or \
-                            '/' in self.screen.get() or '**' in self.screen.get():
+                    screen_get_tot = self.calculate_factorial(self.screen.get())
+                elif self.screen.get()[len(self.screen.get()) - 1].isnumeric() or \
+                        self.screen.get()[len(self.screen.get()) - 1] == ')' and '√' not in self.screen.get():
+                    if not self.screen.get().isnumeric():
                         if '/' in self.screen.get():
                             if self.screen.get().split('/')[1] == '0' and len(self.screen.get().split('/')[1]) == 1:
                                 screen_get_tot = 'Error'
@@ -172,7 +186,7 @@ class Calculador:
                         else:
                             screen_get_tot = eval(self.screen.get())
                 elif self.screen.get()[len(self.screen.get()) - 1] == '%':
-                    screen_get_tot = self.calculate_percentage()
+                    screen_get_tot = self.calculate_percentage(self.screen.get())
                 else:
                     screen_get_tot = 'Error'
             else:
@@ -180,12 +194,12 @@ class Calculador:
 
             if screen_get_tot == 'Error':
                 self.screen.set(screen_get_tot)
-                add_historic(str(self.historic) + ' = Error')
+                add_historic(str(historic) + ' = Error')
             else:
                 if len(str(screen_get_tot)) > 15:
                     self.screen.set(str(screen_get_tot)[0:15] + 'e')
                     self.control.set(str(screen_get_tot)[0:15] + 'e')
-                    add_historic(str(self.historic) + ' = ' + str(screen_get_tot))
+                    add_historic(str(historic) + ' = ' + str(screen_get_tot))
                 else:
                     self.screen.set(str(screen_get_tot))
-                    add_historic(str(self.historic) + ' = ' + str(screen_get_tot))
+                    add_historic(str(historic) + ' = ' + str(screen_get_tot))
