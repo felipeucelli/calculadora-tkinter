9036725859d67f81dac1e88ac2c53a4f39135468
felipeucelli <78266261+felipeucelli@users.noreply.github.com>
2021-02-22 21:14:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# @autor: Felipe Ucelli
+# @github: github.com/felipeucelli
+
+# Built-in
 from tkinter import Tk
+
+# Módulo próprio
 from app.calculadora import Calculadora
 
 if __name__ == '__main__':
